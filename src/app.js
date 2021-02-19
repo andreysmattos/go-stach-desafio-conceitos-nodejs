169d73aa583fe3b6793cabc1b1df16567dbe08cd
Andrey Mattos <andreysmattos@hotmail.com>
2021-02-18 22:52:26 -0300

    feat(all) all tests o

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  
+  const id = uuid();
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title, url, techs} = request.body;
+  let index = repositories.findIndex(repository => repository.id === id);
+  if(index === -1) return response.status(400).send(); 
+
+    repositories[index] = {
+      ...repositories[index],
+      title,
+      url,
+      techs      
+    }
+    
+
+    return response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  let index = repositories.findIndex(repository => {
+    return repository.id === id
+  });
+
+  if( index === -1) return response.status(400).send();
+
+  repositories.splice(index, 1);
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+    
+  let index = repositories.findIndex(repository => repository.id === id);
+  
+
+  if(index === -1 ) return response.status(400).send();
+
+  repositories[index] = {...repositories[index], likes: ++repositories[index].likes};
+
+  return response.status(201).json(repositories[index]);
+  
 });
 
 module.exports = app;
